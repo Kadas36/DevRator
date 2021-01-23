a0ce42c86e810e10d8ba2057023137fe95e32f2f
kadas36 <oduorthomas14@gmail.com>
2021-01-23 17:00:46 +0300

    Add Random lead image

--- dev/views.py
+++ dev/views.py
@@ -1,4 +1,5 @@
-from dev.models import Profile
+from django.db.models.expressions import Random
+import random
 from django.shortcuts import render
 from .models import Profile,Project,Review
 from django.contrib.auth.decorators import login_required
@@ -8,9 +9,13 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def dev_home(request):
     projects = Project.objects.all()
+    lead_projects = Project.objects.order_by("?")
+    lead_project = lead_projects.first()
+    print(lead_project)
 
     context = {
-        'projects': projects
+        'projects': projects,
+        'lead_project': lead_project
     }
 
     return render(request, 'dev/home.html', context)
