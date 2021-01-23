50dcb2539d556c3204b6a67600195a5e8c3f32b9
kadas36 <oduorthomas14@gmail.com>
2021-01-23 15:12:43 +0300

    Add project querry

--- dev/views.py
+++ dev/views.py
@@ -1,11 +1,17 @@
 from dev.models import Profile
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from .models import Profile,Project,Review
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required(login_url='/accounts/login/')
 def dev_home(request):
-    return render(request, 'dev/home.html')
+    projects = Project.objects.all()
 
+    context = {
+        'projects': projects
+    }
 
-# class dev_home(ListView):
-#     model = Profile
+    return render(request, 'dev/home.html', context)
+
